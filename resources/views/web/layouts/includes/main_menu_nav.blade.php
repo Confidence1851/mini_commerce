e38130889550e5f63f9b23a1cf2f2d1430443932
Eazygee <82817023+Eazygee@users.noreply.github.com>
2022-01-27 23:15:37 +0100

    Added about-page

--- resources/views/web/layouts/includes/main_menu_nav.blade.php
+++ resources/views/web/layouts/includes/main_menu_nav.blade.php
@@ -5,5 +5,6 @@
         </li>
         <li><a href="{{ route("web.shop.index")}}">Shop</a></li>
         <li><a href="{{ route("web.contact_us")}}">Contact us</a></li>
+        <li><a href="{{route("web.about")}}">About us</a></li>
     </ul>
 </nav>
